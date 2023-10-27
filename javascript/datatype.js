3a47efaf10d1862f497a91c64ece744e644b8748
udeannn <udinaryansah123@gmail.com>
2023-10-27 23:16:59 +0700

    make datatype example in javascript

---
+++ javascript/datatype.js
@@ -0,0 +1,20 @@
+// Numbers:
+let length = 10;
+let weight = 80.5;
+
+// Strings:
+let furniture = "Chair";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"Chris", lastName:"John"};
+
+// Array object:
+const country = ["Indonesia", "Kamboja", "Thailand"];
+
+// Date object:
+const date = new Date("2022-03-25");
