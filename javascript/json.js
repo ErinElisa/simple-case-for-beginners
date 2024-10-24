e8d5028d766713a52df2d7631d368f45e2d457fb
udin-hyperdata <udin@hyperdata.biz>
2024-10-24 22:06:05 +0700

    make simple example of json in javascript

---
+++ javascript/json.js
@@ -0,0 +1,18 @@
+
+// JSON (JavaScript Object Notation) is a lightweight data format used for storing and exchanging data, primarily in web applications. 
+// JSON represents data as key-value pairs, making it easy to read for both humans and machines. 
+// It is commonly used for communication between a server and a client due to its simplicity and ease of parsing across different programming languages.
+
+
+// Example JSON fromat:
+let json = {
+    "name": "John",
+    "age": 30,
+    "isStudent": false,
+    "skills": ["JavaScript", "HTML", "CSS"],
+    "address": {
+        "city": "New York",
+        "zipcode": "10001"
+    }
+}
+console.log(json)
